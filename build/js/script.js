76d06c6f0fbd59cb18729fd1475a9f708cd760f6
Дмитрий Жур <tehzienergy@gmail.com>
2016-11-25 21:18:59 +0300

    desktop fixes

--- build/js/script.js
+++ build/js/script.js
@@ -34,6 +34,7 @@ $(document).ready(function(){
 
 
 
+$(".slider").slick();
 
 
 
@@ -43,4 +44,4 @@ $(document).ready(function(){
 
 
 
-})
+$("a[href*=#]").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")||location.hostname==this.hostname){var t=$(this.hash);if(t=t.length?t:$("[name="+this.hash.slice(1)+"]"),t.length)return $("html,body").animate({scrollTop:t.offset().top},1e3),!1}});})
