a5c851890657c11652c8b07c8e0f0219536c96e9
Дмитрий Жур <tehzienergy@gmail.com>
2016-11-26 15:34:51 +0300

    add map

--- build/js/script.js
+++ build/js/script.js
@@ -45,4 +45,4 @@ $(".slider").slick();
 
 
 
-$("a[href*=#]").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")||location.hostname==this.hostname){var t=$(this.hash);if(t=t.length?t:$("[name="+this.hash.slice(1)+"]"),t.length)return $("html,body").animate({scrollTop:t.offset().top},1e3),!1}});})
+$("a[href*=#]").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")||location.hostname==this.hostname){var e=$(this.hash);if(e=e.length?e:$("[name="+this.hash.slice(1)+"]"),e.length)return $("html,body").animate({scrollTop:e.offset().top},1e3),!1}}),window.initMap=function(){var e={lat:59.9616116,lng:30.3164647},a=new google.maps.Map(document.getElementById("map"),{zoom:17,center:e,scrollwheel:!1,scaleControl:!0});new google.maps.Marker({map:a,position:{lat:59.9624924,lng:30.3212176},icon:"img/marker.svg"})};})
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -10,3 +10,20 @@ $('a[href*=#]').click(function () {
     }
   }
 });
+
+window.initMap = function() {
+  var myLatLng = {lat: 59.9616116, lng: 30.3164647};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: myLatLng,
+//    disableDefaultUI: true,
+    scrollwheel: false,
+    scaleControl: true,
+  });
+
+  var marker = new google.maps.Marker({
+    map: map,
+    position: {lat: 59.9624924, lng: 30.3212176},
+    icon: 'img/marker.svg',
+  });
+};
