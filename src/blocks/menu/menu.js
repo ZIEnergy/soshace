f83c598708ac2e819a99377773aca0089d166ef8
Дмитрий Жур <tehzienergy@gmail.com>
2016-11-28 14:33:50 +0300

    fixes 2

--- src/blocks/menu/menu.js
+++ src/blocks/menu/menu.js
@@ -1,4 +1,6 @@
 $('.menu__button').click(function(e) {
   e.preventDefault();
   $('.menu__links').slideToggle('fast').css('display', 'flex');
+  $(this).toggleClass('menu__button--active');
+  $('.header').toggleClass('header--active');
 });
