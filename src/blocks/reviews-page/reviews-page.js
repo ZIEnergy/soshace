36a11d15729d5b1dd72ba40c3ed693a635fb21b2
Дмитрий Жур <tehzienergy@gmail.com>
2016-11-30 02:09:20 +0300

    fix sliders responsive

--- src/blocks/reviews-page/reviews-page.js
+++ src/blocks/reviews-page/reviews-page.js
@@ -1,7 +1,16 @@
-if ($(window).width() < 901) {
-  $('.reviews-page__content').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true
-  });
-}
+$(window).resize(function() {
+  if ($(window).width() < 961) {
+    if (!$('.reviews-page__content').hasClass('slick-initialized')) {
+      $('.reviews-page__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true
+      });
+    }
+  }
+  else {
+    if ($('.reviews-page__content').hasClass('slick-initialized')) {
+      $('.reviews-page__content').slick('unslick');
+    }
+  }
+});
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -22,34 +22,21 @@ if ($(window).width() < 1200) {
   });
 }
 
-//$(function () {
-//    $(window).bind('resize', function () {
-//      if ($(window).width() < 1200 && $(window).width() > 900) {
-//        resizeMe();
-//      }
-//    }).trigger('resize');
-//});
-//
-//var resizeMe = function () {
-//    //Standard width, for which the body font size is correct
-//    var preferredWidth = 1280;
-//    //Base font size for the page
-//    var fontsize = 10;
-//
-//    var displayWidth = $(window).width();
-//    var percentage = displayWidth / preferredWidth;
-//    var newFontSize = fontsize * percentage;
-//    $("html").css("font-size", newFontSize);
-//};
-
 window.initMap = function() {
-  var myLatLng = {lat: 59.9616116, lng: 30.3164647};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
-    center: myLatLng,
+    center: {lat: 59.9616116, lng: 30.3164647},
     scrollwheel: false,
     scaleControl: true,
   });
+
+  if ($(window).width() < 960) {
+    map.setCenter({lat: 59.9613295, lng: 30.3210396})
+  };
+
+  if ($(window).width() < 700) {
+    map.setCenter({lat: 59.9623295, lng: 30.3210396})
+  };
 
   var marker = new google.maps.Marker({
     map: map,
