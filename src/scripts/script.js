0d71ecfe65780d6f52815cfbd832555b26f93d07
Дмитрий Жур <tehzienergy@gmail.com>
2016-11-30 13:30:08 +0300

    fixed map, etc.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -21,26 +21,3 @@ if ($(window).width() < 1200) {
     }
   });
 }
-
-window.initMap = function() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 17,
-    center: {lat: 59.9616116, lng: 30.3164647},
-    scrollwheel: false,
-    scaleControl: true,
-  });
-
-  if ($(window).width() < 960) {
-    map.setCenter({lat: 59.9613295, lng: 30.3210396})
-  };
-
-  if ($(window).width() < 700) {
-    map.setCenter({lat: 59.9623295, lng: 30.3210396})
-  };
-
-  var marker = new google.maps.Marker({
-    map: map,
-    position: {lat: 59.9624924, lng: 30.3212176},
-    icon: 'img/marker.svg',
-  });
-};
